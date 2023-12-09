937618bfd8de5b82a4f328c999e0d952fea3dc51
Yovanny Diaz <yjdiazs@falabella.cl>
2023-12-08 23:08:06 -0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thebigyovadiaz/calculator
+
+go 1.20
---
+++ sum.go
@@ -0,0 +1,30 @@
+package calculator
+
+var logMessage = "[CALCULATOR LOG]"
+
+// Version of the calculator
+var Version = "1.0"
+
+func internalSum(number int) int {
+	return number - 1
+}
+
+// Sum two integer numbers
+func Sum(number1, number2 int) int {
+	return number1 + number2
+}
+
+// Subs two integer numbers
+func Subs(number1, number2 int) int {
+	return number1 - number2
+}
+
+// Subs two integer numbers
+func Mul(number1, number2 int) int {
+	return number1 - number2
+}
+
+// Subs two integer numbers
+func Div(number1, number2 int) int {
+	return number1 - number2
+}
